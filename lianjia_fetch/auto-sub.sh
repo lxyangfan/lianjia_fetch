56ff51956392d9f2987d880ce2ef935cbdc5ae47
lxyangfan <lxyangfan@gmail.com>
2017-07-12 14:22:20 +0800

    Auto Commit from AWS

---
+++ lianjia_fetch/auto-sub.sh
@@ -0,0 +1,7 @@
+echo "Start to commit and push..."
+
+git add . 
+git commit -m "Auto Commit from AWS"
+git push origin linuxDev
+
+echo "Commit and push successfully..."
--- lianjia_fetch/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Clean logs"
-echo "" > logs.txt
-echo "" > time.txt
-sudo chmod 777 logs.txt time.txt
-echo "Restart Cron service"
-sudo service cron restart
-echo "Restart finished"
