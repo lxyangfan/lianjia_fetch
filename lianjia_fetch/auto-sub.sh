5b9cf69a5cdde014c3e0dc8078cc5f492d73cfd0
lxyangfan <lxyangfan@gmail.com>
2017-07-12 15:01:11 +0800

    Auto Commit from AWS

--- lianjia_fetch/auto-sub.sh
+++ lianjia_fetch/auto-sub.sh
@@ -1,7 +1,12 @@
 echo "Start to commit and push..."
-
+echo 1
 git add . 
+echo 2
+sleep 2
 git commit -m "Auto Commit from AWS"
+echo 3
+sleep 3
 git push origin linuxDev
+echo 4
 sleep 4
 echo "Commit and push successfully..."
